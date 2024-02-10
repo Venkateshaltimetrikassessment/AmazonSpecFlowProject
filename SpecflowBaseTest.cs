ab56067511a46b53128de93feb92fb47b9f4fb65
Venkateshaltimetrikassessment <159566009+Venkateshaltimetrikassessment@users.noreply.github.com>
2024-02-10 10:45:23 +0530

    Add files via upload

---
+++ SpecflowBaseTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1
+{
+    [Binding]
+    public class SpecflowBaseTest : TechTalk.SpecFlow.Steps
+    {
+        protected IWebDriver Driver { get; }
+        //protected LoginPage LoginPage { get; }
+
+        /*public SpecflowBaseTest()
+        {
+        }*/
+
+        public SpecflowBaseTest(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void NavigateToURL(string URL)
+        {
+            Driver.Navigate().GoToUrl(URL);
+        }
+    }
+}
